aa2740c33d21151f174f32137516243dfba8ffd6
SLOW-PC\ved <vkashyap4@sapient.com>
2017-09-04 19:27:02 +0530

    made some responsive layout using grid

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,40 +1,60 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import './index.css';
 import Dashboard from '../Dashboard';
 class App extends Component {
-  render() {
-    return (
-      <div className='app-wrapper'>
-        <Dashboard>
-        </Dashboard>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='app-wrapper'>
+				<Dashboard>
+				</Dashboard>
+				<Navbar inverse fixedTop>
+					<Grid>
+						<Navbar.Header>
+							<Navbar.Brand>
+								<a href="/">React App</a>
+							</Navbar.Brand>
+							<Navbar.Toggle />
+						</Navbar.Header>
+					</Grid>
+				</Navbar>
+				<Jumbotron>
+					<Grid>
+						<Row className="show-grid">
+							<Col xs={12} sm={6} md={4} lg={3} >
+								<div className="btn-col">
+									<Button bsStyle="danger" bgSize='small'>
+										Danger
+									</Button>
+								</div>
+							</Col>
+							<Col xs={12} sm={6} md={4} lg={3}>
+								<div className="btn-col">
+									<Button bsStyle="success" bgSize='small'>
+										Success
+									</Button>
+								</div>
+							</Col>
+							<Col xs={12} sm={6} md={4} lg={3} >
+								<div className="btn-col">
+									<Button bsStyle="info" bgSize='small'>
+										Info
+									</Button>
+								</div>
+							</Col>
+							<Col xs={12} sm={6} md={4} lg={3} >
+								<div className="btn-col">
+									<Button bsStyle="default" bgSize='small'>
+										Default
+									</Button>
+								</div>
+							</Col>
+						</Row>
+					</Grid>
+				</Jumbotron>
+			</div>
+		);
+	}
 }
 
 export default App;
